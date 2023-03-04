c56223c80cf41e4cb78bd792c9d29a19370f1423
shabi9605 <83868130+shabi9605@users.noreply.github.com>
2023-03-04 21:00:55 +0530

    Update settings.py

--- data_hiding/settings.py
+++ data_hiding/settings.py
@@ -144,6 +144,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 EMAIL_HOST  = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'shabi960580@gmail.com'
-EMAIL_HOST_PASSWORD = 'sepdqshuivtwlsjp'
+EMAIL_HOST_PASSWORD = ''
 EMAIL_USE_TLS = True
 EMAIL_USE_SSL = False
